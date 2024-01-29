3f6f0e240e874865705649de37980c0fb7d7f68e
Monocursive <contact@monocursive.com>
2024-01-29 14:46:23 +0100

    init

---
+++ lib/ueberauth_tiktok.ex
@@ -0,0 +1,18 @@
+defmodule UeberauthTiktok do
+  @moduledoc """
+  Documentation for `UeberauthTiktok`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UeberauthTiktok.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
